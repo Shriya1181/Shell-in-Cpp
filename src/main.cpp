ccb3221a3b75b31c66b3254299f968e9f1855d9f
Shriya1181 <shriyaa@gmail.com>
2026-09-15 15:49:14 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
 #include <set>
-#include <cstdlib>
+#include <cstdlib> // Required for std::getenv (from C library)
+#include <sstream>
+#include <filesystem> // Required for checking files and permissions
+#include <vector>
+
+namespace fs = std::filesystem; // Alias for easier access to filesystem namespace
 
 #ifdef _WIN32
 constexpr char PATH_LIST_SEPARATOR = ';';
@@ -17,6 +22,14 @@ int main() {
   //getenv returns a pointer to the value in the environment, or NULL if there is no match. 
   //For PATH this returns a pointer to the value of the PATH environment variable, a list of directories separated by PATH_LIST_SEPARATOR. 
   char* path_env = std::getenv("PATH");
+  //convert pointer to string and then use stringstream to split the string into list of directories based on the PATH_LIST_SEPARATOR(:) in path_dirs
+  std::string path_str(path_env); //converts to C++ style string
+  std::stringstream ss(path_str); //converts to stringstream (easy to split)
+  std::string directory;
+  std::vector<std::string> path_dirs;
+  while (std::getline(ss, directory, PATH_LIST_SEPARATOR)) {
+        path_dirs.push_back(directory);
+    }
   while(true) {
     std::string command;
     // TODO: Uncomment the code below to pass the first stage
@@ -28,17 +41,26 @@ int main() {
       //break exits the while loop and terminates the program or can use return 0
       break;
     //substr returns a substring of the string starting from the index specified and of length specified
-    } else if(command.substr(0, 5) == "echo ") {
+    } else if(command.size() >= 5 && command.substr(0, 5) == "echo ") {
       std::cout << command.substr(5) << std::endl;
     } else if(command.substr(0, 5) == "type ") {
       std::string cmd = command.substr(5);
       if (builtins.count(cmd)) {
         std::cout << cmd << " is a shell builtin" <<std::endl;
       } else {
-        if (path_env) {
-          
+        bool found = false;
+        for (const std::string& dir : path_dirs) {
+          fs::path full_path = fs::path(dir) / cmd;
+          if (fs::exists(full_path) && fs::is_regular_file(full_path)) {
+            std::cout << cmd << " is " << full_path.string() << std::endl;
+            found = true;
+            break; // Stop searching once we find the first match
+          }
         }
-        std::cerr << cmd << ": not found" << std::endl;
+        //If could'nt find in any of the directories in PATH, print not found message to stdout
+        if (!found) {
+          std::cout << cmd << ": not found" << std::endl; // Codecrafters expects stdout for this test stage
+        }
       }
     } else {
     std::cerr << command << ": command not found" << std::endl; 
