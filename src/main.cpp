516cc15c0dbd1ab3b1624267d9e02655635f899e
Shriya1181 <shriyaa@gmail.com>
2026-09-15 13:53:22 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,18 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  std::string command;
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
-  //cin reads input till the first whitespace character while getline reads the entire line including whitespace characters
-  std::getline(std::cin, command);
-  std:: cerr << command << ": command not found"; 
+  while(true) {
+    std::string command;
+    // TODO: Uncomment the code below to pass the first stage
+    std::cout << "$ ";
+    //cin reads input till the first whitespace character
+    //getline reads the entire line including whitespace characters
+    std::getline(std::cin, command);
+    std:: cerr << command << ": command not found" << std::endl; 
+  }
+
 }
+
+//REPL code loop - Read Evaluate Print Loop is the cycle the shell follows till it is terminated.
+//After it evaluates a command, it prints error (as of now) and waits for next command with $
+
